0d9e0fe0b8a685a5ab80e02941f5dd9ecbc486f5
Bhakti Shukla <bshukla@brightsign.biz>
2025-12-03 17:14:17 -0800

    Fix pkg-dev.sh script and run dos2unix

--- examples/common-scripts/pkg-dev.sh
+++ examples/common-scripts/pkg-dev.sh
@@ -9,10 +9,10 @@ ext_name="$3"
 ext_folder_path=$(find ../ -maxdepth 1 -type d -name "${ext_name}*" | head -n 1)
 
 # shift to target directory to squash
-cd $1
+cd ${ext_folder_path}/$1
 
 ${script_dir}/make-extension-$2 $3
 
-zip -r ${ext_folder_path}/${ext_name}-$(date +%s).zip ext_${ext_name}*
+zip -r ../${ext_name}-$(date +%s).zip ext_${ext_name}*
 
 rm -rf ext_${ext_name}*
